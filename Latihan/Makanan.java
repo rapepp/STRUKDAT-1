0de5c9d72fa9cd3166afc155e6d7fdb5ef06ab8e
rapepp <108265619+rapepp@users.noreply.github.com>
2023-03-09 10:20:47 +0700

    Add files via upload

---
+++ Latihan/Makanan.java
@@ -0,0 +1,8 @@
+package Latihan;
+
+public class Makanan extends Hidangan {
+    public String disantap(){
+        return this.getNamaHidangan() + " dimakan";
+    }
+    
+}
---
+++ Latihan/Minuman.java
@@ -0,0 +1,7 @@
+package Latihan;
+
+public class Minuman extends Hidangan {
+    public String disantap(){
+        return this.getNamaHidangan() + " diminum";
+    }
+}
